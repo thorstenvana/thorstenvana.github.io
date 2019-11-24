bd546348020c1d9b087da1a38a1828fd6861b0ab
Thorsten Vana <thorstenvana@hotmail.com>
2019-11-24 19:25:31 +0000

    Header image fixed and skills section added

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
 function resizeDiv(maindiv,subdiv) {
 	vpw = $(window).width();
-	vph = $(window).height();
-	$(maindiv).css({'height': vph + 'px'});
 	$(maindiv).css({'width': vpw + 'px'});
 	$(maindiv).css({'top': '0'});
 	$(subdiv).css({'top': '40%'});
-	$('.wrapper').css({'marginTop': vph + 'px'});
+	$('.wrapper').css({'marginTop': $(maindiv).height() + 'px'});
 }
